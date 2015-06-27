a08682b56c2201469791c81d661af2712004352f
packyzhou <packyzhou@icloud.com>
2015-06-27 18:07:22 +0800

    PKLormapping 1.1version
    1.1版本
    修复：
    	属性空值SQL映射失败BUG
    
    新增：
    	新增多表联动映射
    	新增事务处理机制
    	新增批量处理API，实测万级数据插入快3倍以上

---
+++ ORM框架/真机版本/PKLormapping.framework/Headers/PKAccessThreadProtocol.h
@@ -0,0 +1,74 @@
+//
+//  PKAccessThreadProtocol.h
+//  PKlormTest
+//
+//  Created by 周经伟 on 15/6/26.
+//  Copyright (c) 2015年 packy. All rights reserved.
+//
+
+#ifndef PKlormTest_PKAccessThreadProtocol_h
+#define PKlormTest_PKAccessThreadProtocol_h
+
+@protocol PKAccessThreadProtocol
+
+/*
+ *  多线程操作数据库初始化
+ *  path为数据库路径
+ */
+-(id) initWithSQLitePath:(NSString *) path;
+
+/*
+ * 创建通用数据库的线程，并加入到线程池
+ * sql为执行sql
+ * delegate 通过代理返回结果集
+ */
+-(void) execute:(NSString *) sql callBackTarget:(id) delegate;
+
+/*
+ * 创建一个访问数据库的线程，并加入到线程池
+ * obj 是注入对象，若请求是select需要添加注入对象
+ * delegate 通过代理返回结果集
+ */
+-(void) queryExecute:(PKHQLer *) hql injectObj:(id) obj callBackTarget:(id) delegate;
+
+/*
+ * 创建一个插入数据库的线程，并加入到线程池
+ * obj 是注入对象，若请求是select需要添加注入对象
+ * delegate 通过代理返回结果集
+ */
+-(void) insertExecute:(id) obj callBackTarget:(id) delegate;
+
+/*
+ * 创建一个修改数据库的线程，并加入到线程池
+ * obj 是注入对象，若请求是select需要添加注入对象
+ * delegate 通过代理返回结果集
+ */
+-(void) updateExecute:(PKHQLer *) hql injectObj:(id) obj callBackTarget:(id) delegate;
+
+/*
+ * 创建一个删除数据库的线程，并加入到线程池
+ * obj 是注入对象，若请求是select需要添加注入对象
+ * delegate 通过代理返回结果集
+ */
+-(void) deleteExecute:(PKHQLer *) hql injectObj:(id) obj callBackTarget:(id) delegate;
+
+/*
+ * 创建一个查询总数数据库的线程，并加入到线程池
+ * delegate 通过代理返回结果集
+ */
+-(void) countExecute:(PKHQLer *) hql injectObj:(id) obj callBackTarget:(id) delegate;
+
+/*
+ *  创建一个批量插入数据库线程，并加入线程池
+ *  batchArray 批量处理数据列表，元素必须为同类型对象
+ *  delegate 通过代理返回结果集
+ */
+-(void) batchInsertExecute:(NSArray *) batchArray callBackTarget:(id) delegate;
+/*
+ *  创建一个批量插入数据库线程，并加入线程池
+ *  batchArray 批量处理数据列表，元素必须为同类型对象
+ *  delegate 通过代理返回结果集
+ */
+-(void) batchUpdateExecute:(PKHQLer *) hql batchArray:(NSArray *) batchArray callBackTarget:(id) delegate;
+@end
+#endif
