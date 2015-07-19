4d18542e62595f142d5f347646adaa5cb8367885
packyzhou <packyzhou@icloud.com>
2015-07-20 00:50:56 +0800

    新增
    1.存增移植Swift版
    2.Swift版PKLormapping框架非桥接，编译速度快

---
+++ ORM框架(Swift版)/模拟器版本/PKLormappingSwift.framework/Headers/PKLormappingSwift.h
@@ -0,0 +1,20 @@
+//
+//  PKLormappingSwift.h
+//  PKLormappingSwift
+//
+//  Created by 周经伟 on 15/7/19.
+//  Copyright (c) 2015年 packy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "sqlite3.h"
+
+//! Project version number for PKLormappingSwift.
+FOUNDATION_EXPORT double PKLormappingSwiftVersionNumber;
+
+//! Project version string for PKLormappingSwift.
+FOUNDATION_EXPORT const unsigned char PKLormappingSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PKLormappingSwift/PublicHeader.h>
+
+
